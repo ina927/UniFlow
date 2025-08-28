3a34a044445604cc7a7c0717879211641127e71b
cjungwo <jwoo516@naver.com>
2025-08-28 16:56:30 +1000

    [fix] fix api response error
    add NextResponse.json() for return value in withDB function
    
    Ref: F100

--- uni-flow/src/shared/api/middlewares/with-db.ts
+++ uni-flow/src/shared/api/middlewares/with-db.ts
@@ -1,4 +1,4 @@
-import type { NextRequest } from 'next/server';
+import { NextResponse, type NextRequest } from 'next/server';
 import { connectDB } from '@/shared/lib/mongoose';
 import { ResponseDto } from '../dto';
 import { ApiParams } from '../models';
@@ -7,13 +7,13 @@ export const withDB = (handler: (req: NextRequest, params?: ApiParams) => Promis
   return async (req: NextRequest, params?: ApiParams) => {
     try {
       await connectDB();
-      return await handler(req, params);
+      return NextResponse.json(await handler(req, params));
     } catch (error) {
       console.error('Database connection error:', error);
-      return {
+      return NextResponse.json({
         status: 500,
         data: "Internal Server Error",
-      };
+      });
     }
   };
 }
