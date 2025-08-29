b28dffe84d7278bd5dc1d87909dbfb0ea9dfddbe
cjungwo <jwoo516@naver.com>
2025-08-30 09:02:25 +1000

    [fix] remove duplicate code in getUsers
    Ref: F100

--- uni-flow/src/entities/users/services/get-users.service.ts
+++ uni-flow/src/entities/users/services/get-users.service.ts
@@ -1,8 +1,6 @@
 import { User } from "@/shared/models";
-import { connectDB } from "@/shared/lib/mongoose";
 
 export const getUsers = async () => {
-  await connectDB();
   const users = await User.find();
   return users;
 };
