08f8c1e16f168b7daa6246666716acc2c978a3fb
cjungwo <jwoo516@naver.com>
2025-09-01 17:36:01 +1000

    [feat] add Header, Navbar, Footer
    add Header layout
    add Navbar layout
    add Footer layout
    add indexs for export
    
    Ref: F103

---
+++ uni-flow/src/widgets/common/ui/Footer.tsx
@@ -0,0 +1,23 @@
+import clsx from 'clsx';
+import Link from 'next/link';
+import Image from 'next/image';
+import { LOGO } from '@/shared/consts/images';
+
+interface Props {
+  className?: string;
+}
+
+export const Footer = (props: Props) => {
+  return (
+    <footer className={clsx(props.className, "bg-[#F9FAFB] flex items-center justify-between w-full h-[48px] px-[16px]")}>
+      <div className="flex items-center justify-start">
+        <Link href="/">
+          <Image src={LOGO.src} alt={LOGO.alt} width={56} height={56} />
+        </Link>
+      </div>
+      <div className="flex items-center justify-center">
+        <p className="text-body1-bold">&copy; {new Date().getFullYear()} UniFlow</p>
+      </div>
+    </footer>
+  );
+};
---
+++ uni-flow/src/widgets/common/ui/Header.tsx
@@ -0,0 +1,35 @@
+import clsx from 'clsx';
+import Link from 'next/link';
+import Image from 'next/image';
+import { LOGO, SETTINGS } from '@/shared/consts/images';
+
+import { HeaderTitle } from '@/features/common';
+
+interface HeaderProps {
+  className?: string;
+}
+
+export const Header = (props: HeaderProps) => {
+  return (
+    <header className={clsx(props.className, "flex flex-row w-full h-[74px] items-center px-[16px]")}>
+      <div className="flex items-center mr-[8px]">
+        <Link href="/">
+          <Image 
+            src={LOGO.src}
+            alt={LOGO.alt}
+            width={68}
+            height={68}
+          />
+        </Link>
+      </div>
+      <div className="flex items-center justify-between ml-[8px] w-full h-[74px] border-b border-b-[#E5E7EB]">
+        <HeaderTitle />
+        <div className="flex items-center mr-[8px]">
+          <Link href="/settings">
+            <Image src={SETTINGS.src} alt={SETTINGS.alt} width={24} height={24} />
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+};
