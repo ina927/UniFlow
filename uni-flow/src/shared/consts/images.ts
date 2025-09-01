9b97835251388956cf70244e09fc1985a1ddc089
cjungwo <jwoo516@naver.com>
2025-09-01 17:30:53 +1000

    [feat] add image consts
    add image interface
    add image consts
    
    Ref: F103

---
+++ uni-flow/src/shared/consts/images.ts
@@ -0,0 +1,76 @@
+export interface IImage {
+  src: string;
+  alt: string;
+  width?: number;
+  height?: number;
+}
+
+export const LOGO: IImage = {
+  src: '/logo.png',
+  alt: 'UniFlow Logo',
+};
+
+export const PROFILE: IImage = {
+  src: '/profile.svg',
+  alt: 'Profile',
+};
+
+export const ACADEMIC: IImage = {
+  src: '/academic.svg',
+  alt: 'Academic',
+}; 
+
+export const RESULTS: IImage = {
+  src: '/results.svg',
+  alt: 'Results',
+};
+
+export const PLANNER: IImage = {
+  src: '/tasks.svg',
+  alt: 'Planner',
+};
+
+export const TIMER: IImage = {
+  src: '/timer.svg',
+  alt: 'Timer',
+};
+
+export const ADMIN: IImage = {
+  src: '/admin.svg',
+  alt: 'Admin',
+};
+
+export const SETTINGS: IImage = {
+  src: '/settings.svg',
+  alt: 'Settings',
+};
+
+export const ARROW_LEFT: IImage = {
+  src: '/arrow-left.svg',
+  alt: 'Arrow Left',
+};
+  
+export const ARROW_RIGHT: IImage = {
+  src: '/arrow-right.svg',
+  alt: 'Arrow Right',
+};
+
+export const NEXT: IImage = {
+  src: '/next.svg',
+  alt: 'Next',
+};
+  
+export const PREV: IImage = {
+  src: '/prev.svg',
+  alt: 'Prev',
+};
+  
+export const DARK_MODE: IImage = {
+  src: '/dark.svg',
+  alt: 'Dark Mode',
+};
+  
+export const LIGHT_MODE: IImage = {
+  src: '/light.svg',
+  alt: 'Light Mode',
+};
