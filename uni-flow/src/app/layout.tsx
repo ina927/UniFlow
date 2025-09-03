5dd5f7d53b35064da727cdc268809f14fc2f68f8
cjungwo <jwoo516@naver.com>
2025-09-04 09:26:37 +1000

    [feat] add query provider
    add query provider for managing server data
    apply query provider in root layout
    
    Ref: F103

--- uni-flow/src/app/layout.tsx
+++ uni-flow/src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased" suppressHydrationWarning={true}>
+      <body className="antialiased py-[16px]" suppressHydrationWarning={true}>
         <QueryProviders>
           <Header />
           <div className="flex flex-row">
