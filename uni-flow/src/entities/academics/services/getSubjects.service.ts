5419af60f4b627799de9d894b528988b10befd55
cjungwo <jwoo516@naver.com>
2025-09-04 14:03:32 +1000

    [fix] add entity for type strict
    add entity for type strict and control error
    
    Ref: F103

--- uni-flow/src/entities/academics/services/getSubjects.service.ts
+++ uni-flow/src/entities/academics/services/getSubjects.service.ts
@@ -1,6 +1,11 @@
 import { Subject } from "@/shared";
+import { SubjectEntity } from "../entities/subject.entity";
 
-export const getSubjects = async (selectedTermId: string) => {
+export const getSubjects = async (selectedTermId: string): Promise<{ data: SubjectEntity[] }> => {
+  if (!selectedTermId) {
+    return { data: [] };
+  }
+  
   const subjects = await Subject.find({ termId: selectedTermId });
-  return subjects;
+  return { data: subjects };
 };
--- uni-flow/src/entities/academics/services/getTerms.service.ts
+++ uni-flow/src/entities/academics/services/getTerms.service.ts
@@ -1,6 +1,11 @@
 import { Term } from "@/shared";
+import { TermEntity } from "../entities/term.entity";
 
-export const getTerms = async (academicCourseId: string) => {
+export const getTerms = async (academicCourseId: string): Promise<{ data: TermEntity[] }> => {
+  if (!academicCourseId) {
+    return { data: [] };
+  }
+  
   const terms = await Term.find({ academicCourseId });
-  return terms;
+  return { data: terms };
 };
