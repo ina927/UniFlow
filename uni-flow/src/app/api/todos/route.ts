4b5154edc71a181cc9b576647464cfe3818969e4
Trisha Andalene <trishaandalene2006@gmail.com>
2025-09-04 13:57:42 +1000

    [feat] milestone checkpoint
    Checkpoint for database editor

--- uni-flow/src/app/api/todos/route.ts
+++ uni-flow/src/app/api/todos/route.ts
@@ -1,8 +1,7 @@
 import { connectDB } from "@/shared";
 import { ToDo } from "@/shared";
-import { ObjectId } from "mongoose";
 
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: { json: () => PromiseLike<{ newToDo: any; }> | { newToDo: any; }; }){
     try {
@@ -12,6 +11,20 @@ export async function POST(request: { json: () => PromiseLike<{ newToDo: any; }>
         const savedToDo = new ToDo({userId, subjectId, assessmentId, title, content, startDate, endDate, taskStatus})
         await savedToDo.save()
         return NextResponse.json(newToDo, {status: 201})
+    } catch (error){
+        console.log(error)
+    }
+}
+
+export async function GET(req: NextRequest) {
+    try {
+        await connectDB();
+
+        const userId = req.headers.get("userId");
+
+        const toDos = await ToDo.find({userId: userId});
+
+        return NextResponse.json(toDos, {status: 200})
     } catch (error){
         console.log(error)
     }
