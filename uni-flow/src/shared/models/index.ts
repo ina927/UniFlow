b0fdebd421385da21cded76516ec50ce32207c20
cjungwo <jwoo516@naver.com>
2025-09-05 17:41:52 +1000

    [ifx] delete mongo schema and mongoose setup
    delete mongo schema and mongoose setup
    
    Ref: F111

--- uni-flow/src/shared/models/index.ts
+++
@@ -1,8 +0,0 @@
-export { User } from "./User";
-export { AcademicCourse } from "./AcademicCourse";
-export { Term } from "./Term";
-export { Subject } from "./Subject";
-export { Assessment } from "./Assessment";
-export { ToDo } from "./ToDo";
-export { Instructor } from "./Instructor";
-export { TimerSession } from "./TimerSession";
