76d5b061e4f80e1f7199fbea3539c1137f9c6369
cjungwo <jwoo516@naver.com>
2025-09-12 12:29:46 +1000

    [feat] add term service in server
    add term service in server for term domain
    
    Ref: F103

--- uni-flow/src/entities/academics/entities/term.entity.ts
+++ uni-flow/src/entities/academics/entities/term.entity.ts
@@ -2,7 +2,10 @@ export interface TermEntity {
   id: string;
   academicCourseId: string;
   title: string;
-  currentAcademicYear?: number;
-  startDate?: Date;
-  endDate?: Date;
+  academicYear?: number | null;
+  startDate?: Date | null;
+  endDate?: Date | null;
+  createdAt: Date;
+  updatedAt: Date;
+  version: number;
 }
--- uni-flow/src/features/academics/api/getTerms.ts
+++
@@ -1,5 +0,0 @@
-import { TERMS_URL } from "../const";
-
-export const getTerms = async (academicCourseId: string) => {
-    return await fetch(`${TERMS_URL}?academicCourseId=${academicCourseId}`).then(res => res.json());
-};
