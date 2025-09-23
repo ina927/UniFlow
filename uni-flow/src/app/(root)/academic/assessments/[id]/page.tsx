376e27273d7589c15911a2f7f58bdb2c1b135aed
ina song <inasong03@gmail.com>
2025-09-23 12:19:16 +1000

    [feat] weight error validation
    id-f105

--- uni-flow/src/app/(root)/academic/assessments/[id]/page.tsx
+++ uni-flow/src/app/(root)/academic/assessments/[id]/page.tsx
@@ -33,6 +33,10 @@ export default function AssessmentDetailPage() {
   // subject + assessments
   const { data: subject } = useSubjectDetailQuery(subjectId);
   const { data: list = [], isLoading } = useAssessmentsQuery(subjectId);
+  const totalWeight = useMemo(
+    () => list.reduce((acc, it) => acc + (Number(it.weight) || 0), 0),
+    [list]
+  );
 
   const item = useMemo(() => list.find((i) => i.id === id) ?? null, [list, id]);
 
@@ -110,6 +114,7 @@ export default function AssessmentDetailPage() {
         onOpenChange={setOpenEdit}
         subjectId={subjectId}
         initial={item}
+        currentTotalWeight={totalWeight}
       />
 
       {/* Delete confirm */}
