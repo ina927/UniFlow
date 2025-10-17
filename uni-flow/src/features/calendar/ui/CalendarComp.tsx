e1977c7ccfe195f3c4964cb3c91b80428c25dd85
VisioneXperienceDeveloper <visionexperiencedeveloper@gmail.com>
2025-10-18 10:06:23 +1100

    [fix] calender infinity api request
    Ref: F108

--- uni-flow/src/features/calendar/ui/CalendarComp.tsx
+++ uni-flow/src/features/calendar/ui/CalendarComp.tsx
@@ -1,51 +1,62 @@
-"use client";
+'use client';
 
 // React import
-import {useEffect, useRef, useState} from "react";
-import { DateSelectArg, EventApi, EventClickArg, EventInput, formatDate } from "@fullcalendar/core/index.js";
-import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from "@fullcalendar/daygrid";
-import timeGridPlugin from "@fullcalendar/timegrid";
-import interactionPlugin from "@fullcalendar/interaction";
+import {
+  DateSelectArg,
+  EventApi,
+  EventClickArg,
+  EventInput,
+  formatDate,
+} from '@fullcalendar/core/index.js';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import FullCalendar from '@fullcalendar/react';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import { useEffect, useRef, useState } from 'react';
 
 // database import
-import { getToDos, getToDosByFilter, getToDosById } from "@/features/calendar/apis/todo.api";
-import { AddToDoForm, EditToDoForm } from "@/features/planner/ui";
-import { ToDoEntity } from "@/entities/todos";
+import { ToDoEntity } from '@/entities/todos';
+import {
+  getToDos,
+  getToDosByFilter,
+  getToDosById,
+} from '@/features/calendar/apis/todo.api';
+import { AddToDoForm, EditToDoForm } from '@/features/planner/ui';
 
-import styles from "./CalendarComp.module.css"
+import styles from './CalendarComp.module.css';
 
 // type setup
 type calendarCompProps = {
-    academicCourseId: string;
-    filterBySubjectId?: string | null;
+  academicCourseId: string;
+  filterBySubjectId?: string | null;
 };
 
 // export functions
 export const CalendarComp = ({
-    academicCourseId, 
-    filterBySubjectId,
+  academicCourseId,
+  filterBySubjectId,
 }: calendarCompProps) => {
+  // list state
+  const [events, setEvents] = useState<ToDoEntity[]>([]);
+  const [calendar, setCalendar] = useState<EventApi[]>([]);
 
-    // list state
-    const [events, setEvents] = useState<ToDoEntity[]>([]);
-    const [calendar, setCalendar] = useState<EventApi[]>([]);
+  const [selectedEvent, setSelectedEvent] = useState<EventApi>();
+  const [selectedEventArg, setSelectedEventArg] = useState<EventClickArg>();
+  const [selectedEventInput, setSelectedEventInput] = useState<EventInput[]>(
+    []
+  );
+  const [selectedEventEntity, setSelectedEventEntity] = useState<ToDoEntity>();
+  const [selectedDate, setSelectedDate] = useState<DateSelectArg | null>(null);
+  const [selectedEventId, setSelectedEventId] = useState<string>();
 
-    const [selectedEvent, setSelectedEvent] = useState<EventApi>();
-    const [selectedEventArg, setSelectedEventArg] = useState<EventClickArg>();
-    const [selectedEventInput, setSelectedEventInput] = useState<EventInput[]>([]);
-    const [selectedEventEntity, setSelectedEventEntity] = useState<ToDoEntity>();
-    const [selectedDate, setSelectedDate] = useState<DateSelectArg | null>(null);
-    const [selectedEventId, setSelectedEventId] = useState<string>();
+  // for trigger
+  const [isEditDialogue, setIsEditDialogue] = useState<boolean>(false);
+  const [isAddDialogue, setIsAddDialogue] = useState<boolean>(false);
 
-    // for trigger
-    const [isEditDialogue, setIsEditDialogue] = useState<boolean>(false);
-    const [isAddDialogue, setIsAddDialogue] = useState<boolean>(false);
+  // reference
+  const calendarRef = useRef<FullCalendar | null>(null);
 
-    // reference
-    const calendarRef = useRef<FullCalendar | null>(null);
-
-    // use handler + refresh rate
+  // use handler + refresh rate
 
   const refresh = async () => {
     // filter check
@@ -62,163 +73,159 @@ export const CalendarComp = ({
 
     setEvents(events);
 
-  const FormattedEventsInput: EventInput[] = events.map((todo: any) => ({
+    const FormattedEventsInput: EventInput[] = events.map((todo: any) => ({
       id: todo.id,
       title: todo.title,
       start: new Date(todo.startDate),
       end: todo.endDate ? new Date(todo.endDate) : undefined,
       allDay: todo.allDay ?? false,
-      extendedProps: {  // Put custom properties here
-          subjectId: todo.subjectId,
-          content: todo.description,
-          status: todo.status
-      }
-  }))
+      extendedProps: {
+        // Put custom properties here
+        subjectId: todo.subjectId,
+        content: todo.description,
+        status: todo.status,
+      },
+    }));
 
     console.log(events);
 
     setSelectedEventInput(FormattedEventsInput);
-
   };
 
-    useEffect(() => {
-        const listings = async () => {
-            const events = await getToDos();
-            setEvents(events);
+  useEffect(() => {
+    const listings = async () => {
+      const events = await getToDos();
+      setEvents(events);
 
-            const FormattedEventsInput: EventInput[] = events.map((todo: any) => ({
-              id: String(todo.id),
-              title: todo.title,
-              content: todo.description,
-              start: new Date(todo.startDate),
-              end: todo.endDate ? new Date(todo.endDate) : undefined,
-              allDay: todo.allDay ?? false
-          }))
+      const FormattedEventsInput: EventInput[] = events.map((todo: any) => ({
+        id: String(todo.id),
+        title: todo.title,
+        content: todo.description,
+        start: new Date(todo.startDate),
+        end: todo.endDate ? new Date(todo.endDate) : undefined,
+        allDay: todo.allDay ?? false,
+      }));
 
-            setSelectedEventInput(FormattedEventsInput);
-        };
-        calendarRef.current?.getApi().removeAllEvents();
-        listings();
-      }, []);
+      setSelectedEventInput(FormattedEventsInput);
+    };
+    calendarRef.current?.getApi().removeAllEvents();
+    listings();
+  }, []);
 
-      useEffect(() => {
-        refresh();
-      }, [events, filterBySubjectId]);
+  useEffect(() => {
+    refresh();
+  }, [events, filterBySubjectId]);
 
-      const handleDateClick = (selected: DateSelectArg) => {
-        setSelectedDate(selected);
-        setIsAddDialogue(true);
-      };
+  const handleDateClick = (selected: DateSelectArg) => {
+    setSelectedDate(selected);
+    setIsAddDialogue(true);
+  };
 
-      const handleEventClick = async (selected: EventClickArg) => {
-        const event = selected.event;
-    
-        const expectedEvent = await getToDosById(event.id);
-        console.log("Keys: " + Object.keys(expectedEvent));
+  const handleEventClick = async (selected: EventClickArg) => {
+    const event = selected.event;
 
-        setSelectedEventEntity(expectedEvent);
-        setSelectedEventArg(selected);
-        setSelectedEvent(selected.event);
-        setSelectedEventId(event.id ?? null);
-        setIsEditDialogue(true);
-      };
+    const expectedEvent = await getToDosById(event.id);
+    console.log('Keys: ' + Object.keys(expectedEvent));
 
-      const handleCloseEdit = () => {
-        setIsEditDialogue(false);
-        setSelectedEvent(undefined);
-        refresh();
-      };
+    setSelectedEventEntity(expectedEvent);
+    setSelectedEventArg(selected);
+    setSelectedEvent(selected.event);
+    setSelectedEventId(event.id ?? null);
+    setIsEditDialogue(true);
+  };
 
-      const handleCloseAdd = () => {
-        setIsAddDialogue(false);
-        refresh();
-      };
-    
+  const handleCloseEdit = () => {
+    setIsEditDialogue(false);
+    setSelectedEvent(undefined);
+    refresh();
+  };
 
-      return (
-        <div
-          className={'flex px-10 justify-start items-start gap-8' + " " + styles.container}
-        >
-          <div
-            className={'w-3/12' + ' ' + styles.divListContainer}
-          >
-            <div className='text-title1-bold'>Upcoming Events</div>
-            <br />
-            <br />
-            <div className={styles.divList}>
-              <ul className='space-y-4'>
-                {events.length <= 0 && (
-                  <div className='italic text-center text-gray-400'>
-                    Nothing in Here Yet..
-                  </div>
-                )}
-                {events.length > 0 &&
-                  events.map((event: ToDoEntity) => (
-                    <li
-                      className='border border-gray-200 shadow px-4 py-2 rounded-md text-blue-800'
-                      key={event.id}
-                    >
-                      {event.title}
-                      <br />
-                      <label className='text-slate-950'>
-                        {formatDate(event.endDate, {
-                          year: 'numeric',
-                          month: 'short',
-                          day: 'numeric',
-                        })}
-                      </label>
-                    </li>
-                  ))}
-              </ul>
-            </div>
-          </div>
-          {/* calendar part */}
-          <div
-            className='w-9/12 mt-8'
-            style={{ width: '55vw' }}
-          >
-            <FullCalendar
-              height={'75vh'}
-              plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-              headerToolbar={{
-                left: 'prev,next today',
-                center: 'title',
-                right: 'dayGridMonth, timeGridWeek, timeGridDay',
-              }}
-              initialView='dayGridMonth'
-              selectable={true}
-              editable={true}
-              selectMirror={true}
-              dayMaxEvents={true}
-              select={handleDateClick}
-              eventClick={handleEventClick}
-              // eventsSet={(events) => setEvents(events)}
-              events={selectedEventInput}
-            />
-          </div>
-          {/* Edit component */}
-          {selectedEventEntity && (
-            <EditToDoForm
-              academicCourseId={academicCourseId}
-              event={selectedEventEntity}
-              isOpen={isEditDialogue}
-              onClose={handleCloseEdit}
-              refresh={refresh}
-            />
-          )}
+  const handleCloseAdd = () => {
+    setIsAddDialogue(false);
+    refresh();
+  };
 
-          {/* Add component */}
-          {
-            <AddToDoForm
-              academicCourseId={academicCourseId}
-              isOpen={isAddDialogue}
-              onClose={handleCloseAdd}
-              refresh={refresh}
-            />
-          }
+  return (
+    <div
+      className={
+        'flex px-10 justify-start items-start gap-8' + ' ' + styles.container
+      }
+    >
+      <div className={'w-3/12' + ' ' + styles.divListContainer}>
+        <div className='text-title1-bold'>Upcoming Events</div>
+        <br />
+        <br />
+        <div className={styles.divList}>
+          <ul className='space-y-4'>
+            {events.length <= 0 && (
+              <div className='italic text-center text-gray-400'>
+                Nothing in Here Yet..
+              </div>
+            )}
+            {events.length > 0 &&
+              events.map((event: ToDoEntity) => (
+                <li
+                  className='border border-gray-200 shadow px-4 py-2 rounded-md text-blue-800'
+                  key={event.id}
+                >
+                  {event.title}
+                  <br />
+                  <label className='text-slate-950'>
+                    {formatDate(event.endDate, {
+                      year: 'numeric',
+                      month: 'short',
+                      day: 'numeric',
+                    })}
+                  </label>
+                </li>
+              ))}
+          </ul>
         </div>
+      </div>
+      {/* calendar part */}
+      <div
+        className='w-9/12 mt-8'
+        style={{ width: '55vw' }}
+      >
+        <FullCalendar
+          height={'75vh'}
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth, timeGridWeek, timeGridDay',
+          }}
+          initialView='dayGridMonth'
+          selectable={true}
+          editable={true}
+          selectMirror={true}
+          dayMaxEvents={true}
+          select={handleDateClick}
+          eventClick={handleEventClick}
+          // eventsSet={(events) => setEvents(events)}
+          events={selectedEventInput}
+        />
+      </div>
+      {/* Edit component */}
+      {selectedEventEntity && (
+        <EditToDoForm
+          academicCourseId={academicCourseId}
+          event={selectedEventEntity}
+          isOpen={isEditDialogue}
+          onClose={handleCloseEdit}
+          refresh={refresh}
+        />
+      )}
 
-      )
-    
-
-}
+      {/* Add component */}
+      {
+        <AddToDoForm
+          academicCourseId={academicCourseId}
+          isOpen={isAddDialogue}
+          onClose={handleCloseAdd}
+          refresh={refresh}
+        />
+      }
+    </div>
+  );
+};
